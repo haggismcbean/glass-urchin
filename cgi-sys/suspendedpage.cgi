f56ca7cd86a2e7ec50704d8602bb6a30d20a80e7
Harry <hbishop11@gmail.com>
2019-08-28 20:13:14 +0200

    glass urchin test2

---
+++ cgi-sys/suspendedpage.cgi
@@ -0,0 +1,83 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html><head>
+  
+  <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
+  <title>Please Contact LinkSkyHosting.com</title>
+
+  
+</head><body style="background-image: url(http://linkskyhosting.com/images/LinkSky-suspendedpage.jpg); color: white; background-color: rgb(255, 255, 255);" alink="#ffffcc" link="#ffffcc" vlink="#ffffcc">
+<br>
+
+<br>
+<br>
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<table style="text-align: left; width: 1077px; height: 402px;" border="0" cellpadding="6" cellspacing="6">
+
+  <tbody>
+    <tr>
+      <td style="vertical-align: top; width: 163px; height: 32px;"><br>
+      </td>
+      <td colspan="1" rowspan="1" style="vertical-align: top; text-align: left; width: 887px; height: 32px; font-family: Times New Roman,Times,serif;"><font style="color: white; font-family: Verdana; font-weight: bold;" size="+1">Sorry, this
+site is temporarily offline.</font><br>
+      </td>
+    </tr>
+    <tr>
+      <td style="vertical-align: top; width: 163px; height: 268px;"><font size="-1"><br>
+      </font> </td>
+      <td style="vertical-align: top; width: 887px; font-family: Arial; height: 268px;"><font style="color: white; font-family: Times New Roman,Times,serif;"><font style="font-style: italic; color: rgb(255, 255, 153); font-family: Verdana;">Any
+issues causing this site to go offline can usually be quickly resolved.</font></font><font style="color: white; font-family: Times New Roman,Times,serif;"><br>
+      <br>
+      <font size="-1"><span style="font-family: Verdana;">If you are
+the hosting account owner, first please make sure your payment
+information is up to date by logging in here:</span><br style="font-family: Verdana;">
+      <a style="font-weight: bold; color: rgb(255, 255, 153); font-family: Verdana;" href="https://www.linksky.com/main/bsyst/clientarea.php" target="_top">MEMBER
+BILLING SERVICES</a></font> </font><font style="color: white; font-family: Verdana;" size="-1"><br>
+      <br>
+      </font><font style="color: white; font-family: Verdana;" size="-1">If
+you believe there may be a policy issue, please contact LinkSky Hosting
+by entering a Support Desk request here:<br>
+      <a style="font-weight: bold; color: rgb(255, 255, 153);" href="https://linksky.zendesk.com/tickets/new" target="_top">LinkSky SUPPORT DESK</a></font><font style="color: white; font-family: Verdana;" size="-1"><br>
+      <br>
+      </font><font style="color: white; font-family: Verdana;" size="-1">Or
+you can also contact us by using one of the other contact references in
+the LinkSky Hosting Support Center:<br>
+      <a style="font-weight: bold; color: rgb(255, 255, 153);" href="http://www.linkskyhosting.com/index.php/response-support" target="_top">LinkSky SUPPORT CENTER</a></font><font style="color: white; font-family: Arial;" size="-1"><br>
+      </font><br>
+      <div style="text-align: right;"><span style="font-style: italic;"><font style="font-family: Verdana;">Support Management, LinkSky Value Host
+Inc.</font><br style="font-family: Verdana;">
+      <br style="font-family: Verdana;">
+      <font size="-1"><span style="font-family: Verdana;">visit - </span><a style="color: rgb(255, 255, 153); font-family: Verdana;" href="http://www.linkskyhosting.com" target="_blank">LinkSkyHosting.com</a></font><br>
+      </span> </div>
+      </td>
+    </tr>
+    <tr>
+      <td style="vertical-align: top; width: 163px;"><br>
+      </td>
+      <td style="vertical-align: top; width: 887px; text-align: right;"><br><br><a href="http://www.linksky.com/main/bsyst/aff.php?aff=002"><img src="http://linkskyhosting.com/affbanners/ad1-468x60.jpg" width="468" height="60" border="0"></a><br>
+      </td>
+    </tr>
+    <tr>
+      <td style="vertical-align: top; width: 163px;"><br>
+      </td>
+      <td style="vertical-align: top; width: 887px;"><br>
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+<br>
+
+<br>
+
+</body></html>
---
+++ wp-content/plugins/nextgen-gallery/xml/media-rss.php
@@ -0,0 +1,131 @@
+<?xml version='1.0' encoding='UTF-8' standalone='yes'?>
+<rss version='2.0' xmlns:media='http://search.yahoo.com/mrss/' xmlns:atom='http://www.w3.org/2005/Atom'>
+	<channel>
+		<generator><![CDATA[NextGEN Gallery [http://nextgen-gallery.com]]]></generator>
+		<title>The Glass Urchin</title>
+		<description>Love, life, and being lost</description>
+		<link><![CDATA[http://glassurchin.nerdcomics.com]]></link>
+		<atom:link rel='self' href='http://glassurchin.nerdcomics.com/wp-content/plugins/nextgen-gallery/xml/media-rss.php' type='application/rss+xml' />
+		<atom:link rel='next' href='http://glassurchin.nerdcomics.com/wp-content/plugins/nextgen-gallery/xml/media-rss.php?show=10&amp;page=1&amp;mode=last_pictures' />
+		<item>
+			<title><![CDATA[covermilkshakes-highways]]></title>
+			<description><![CDATA[The cover of the first compilation of comics from The Glass Urchin, Milkshakes & Highways.  It features Ami and Mike in the foreground and ten other characters in the background.  The location is based on a real bar located in Jersey City.]]></description>
+			<link><![CDATA[http://glassurchin.nerdcomics.com/wp-content/gallery/guart/covermilkshakes-highways.png]]></link>
+			<guid>image-id:32</guid>
+			<media:content url='http://glassurchin.nerdcomics.com/wp-content/gallery/guart/covermilkshakes-highways.png' medium='image' />
+			<media:title><![CDATA[covermilkshakes-highways]]></media:title>
+			<media:description><![CDATA[The cover of the first compilation of comics from The Glass Urchin, Milkshakes & Highways.  It features Ami and Mike in the foreground and ten other characters in the background.  The location is based on a real bar located in Jersey City.]]></media:description>
+			<media:thumbnail url='http://glassurchin.nerdcomics.com/wp-content/gallery/guart/thumbs/thumbs_covermilkshakes-highways.png' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) The Glass Urchin (http://glassurchin.nerdcomics.com)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[season_3_winterice]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://glassurchin.nerdcomics.com/wp-content/gallery/auiart/season_3_winterice.jpg]]></link>
+			<guid>image-id:31</guid>
+			<media:content url='http://glassurchin.nerdcomics.com/wp-content/gallery/auiart/season_3_winterice.jpg' medium='image' />
+			<media:title><![CDATA[season_3_winterice]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://glassurchin.nerdcomics.com/wp-content/gallery/auiart/thumbs/thumbs_season_3_winterice.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) The Glass Urchin (http://glassurchin.nerdcomics.com)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[housewatch]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://glassurchin.nerdcomics.com/wp-content/gallery/auiart/housewatch.png]]></link>
+			<guid>image-id:30</guid>
+			<media:content url='http://glassurchin.nerdcomics.com/wp-content/gallery/auiart/housewatch.png' medium='image' />
+			<media:title><![CDATA[housewatch]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://glassurchin.nerdcomics.com/wp-content/gallery/auiart/thumbs/thumbs_housewatch.png' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) The Glass Urchin (http://glassurchin.nerdcomics.com)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[smoothered]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://glassurchin.nerdcomics.com/wp-content/gallery/auiart/smoothered.jpg]]></link>
+			<guid>image-id:29</guid>
+			<media:content url='http://glassurchin.nerdcomics.com/wp-content/gallery/auiart/smoothered.jpg' medium='image' />
+			<media:title><![CDATA[smoothered]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://glassurchin.nerdcomics.com/wp-content/gallery/auiart/thumbs/thumbs_smoothered.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) The Glass Urchin (http://glassurchin.nerdcomics.com)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[beautiful_world]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://glassurchin.nerdcomics.com/wp-content/gallery/auiart/beautiful_world.jpg]]></link>
+			<guid>image-id:28</guid>
+			<media:content url='http://glassurchin.nerdcomics.com/wp-content/gallery/auiart/beautiful_world.jpg' medium='image' />
+			<media:title><![CDATA[beautiful_world]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://glassurchin.nerdcomics.com/wp-content/gallery/auiart/thumbs/thumbs_beautiful_world.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) The Glass Urchin (http://glassurchin.nerdcomics.com)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[fushfush]]></title>
+			<description><![CDATA[Things get curiouser and curiouser for Arisu of the Mysterious Country (translation of the Japanese title of Alice in Wonderland).  The inks for this were auction at the first Asian American Comic Con!]]></description>
+			<link><![CDATA[http://glassurchin.nerdcomics.com/wp-content/gallery/auiart/fushfush.jpg]]></link>
+			<guid>image-id:27</guid>
+			<media:content url='http://glassurchin.nerdcomics.com/wp-content/gallery/auiart/fushfush.jpg' medium='image' />
+			<media:title><![CDATA[fushfush]]></media:title>
+			<media:description><![CDATA[Things get curiouser and curiouser for Arisu of the Mysterious Country (translation of the Japanese title of Alice in Wonderland).  The inks for this were auction at the first Asian American Comic Con!]]></media:description>
+			<media:thumbnail url='http://glassurchin.nerdcomics.com/wp-content/gallery/auiart/thumbs/thumbs_fushfush.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) The Glass Urchin (http://glassurchin.nerdcomics.com)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[happythoughts_1280x800]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://glassurchin.nerdcomics.com/wp-content/gallery/guart/happythoughts_1280x800.jpg]]></link>
+			<guid>image-id:24</guid>
+			<media:content url='http://glassurchin.nerdcomics.com/wp-content/gallery/guart/happythoughts_1280x800.jpg' medium='image' />
+			<media:title><![CDATA[happythoughts_1280x800]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://glassurchin.nerdcomics.com/wp-content/gallery/guart/thumbs/thumbs_happythoughts_1280x800.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) The Glass Urchin (http://glassurchin.nerdcomics.com)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[maps]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[http://glassurchin.nerdcomics.com/wp-content/gallery/guart/maps.png]]></link>
+			<guid>image-id:23</guid>
+			<media:content url='http://glassurchin.nerdcomics.com/wp-content/gallery/guart/maps.png' medium='image' />
+			<media:title><![CDATA[maps]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='http://glassurchin.nerdcomics.com/wp-content/gallery/guart/thumbs/thumbs_maps.png' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) The Glass Urchin (http://glassurchin.nerdcomics.com)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[sir_tillehad_auilix]]></title>
+			<description><![CDATA[An entry for the New Yorker's Many Faces of Tilley contest for 2009.  I made a High Tilley in 2008 and I kind of enjoy the exercise, although I didn't win in either year.]]></description>
+			<link><![CDATA[http://glassurchin.nerdcomics.com/wp-content/gallery/auiart/sir_tillehad_auilix.jpg]]></link>
+			<guid>image-id:22</guid>
+			<media:content url='http://glassurchin.nerdcomics.com/wp-content/gallery/auiart/sir_tillehad_auilix.jpg' medium='image' />
+			<media:title><![CDATA[sir_tillehad_auilix]]></media:title>
+			<media:description><![CDATA[An entry for the New Yorker's Many Faces of Tilley contest for 2009.  I made a High Tilley in 2008 and I kind of enjoy the exercise, although I didn't win in either year.]]></media:description>
+			<media:thumbnail url='http://glassurchin.nerdcomics.com/wp-content/gallery/auiart/thumbs/thumbs_sir_tillehad_auilix.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) The Glass Urchin (http://glassurchin.nerdcomics.com)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[skeleton_my_friend]]></title>
+			<description><![CDATA[Waiting for you to call...]]></description>
+			<link><![CDATA[http://glassurchin.nerdcomics.com/wp-content/gallery/auiart/skeleton_my_friend.png]]></link>
+			<guid>image-id:21</guid>
+			<media:content url='http://glassurchin.nerdcomics.com/wp-content/gallery/auiart/skeleton_my_friend.png' medium='image' />
+			<media:title><![CDATA[skeleton_my_friend]]></media:title>
+			<media:description><![CDATA[Waiting for you to call...]]></media:description>
+			<media:thumbnail url='http://glassurchin.nerdcomics.com/wp-content/gallery/auiart/thumbs/thumbs_skeleton_my_friend.png' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) The Glass Urchin (http://glassurchin.nerdcomics.com)]]></media:copyright>
+		</item>
+	</channel>
+</rss>
